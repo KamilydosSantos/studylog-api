6ba0ed6eeec9f07c49eb41b9fc544c53f00b5ba8
Kamily <kamily7803@gmail.com>
2024-09-11 23:08:15 -0300

    feat: add studies migration and model

---
+++ database/migrations/2024_09_12_013217_create_studies.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('studies', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id');
+            $table->dateTime('start_time', precision: 0);
+            $table->dateTime('end_time', precision: 0);
+            $table->time('total_time', precision: 0);
+            $table->timestamps();
+        });
+
+        Schema::table('studies', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('studies');
+    }
+};
